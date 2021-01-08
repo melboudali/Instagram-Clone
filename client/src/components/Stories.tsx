4530d003b65a62692220be68c27e487fa7c82e40
MedElBoudali <med.sneaky@gmail.com>
2021-01-08 22:56:05 +0000

    add stories animation

--- client/src/components/Stories.tsx
+++ client/src/components/Stories.tsx
@@ -7,12 +7,65 @@ const Container = styled.div`
   border: 1px solid #dbdbdb;
   border-radius: 3px;
   margin: 54px 0 16px;
+  overflow: hidden;
+  @keyframes Slider {
+    100% {
+      transform: translateX(-66.6666%);
+    }
+  }
+`;
+
+const Main = styled.div`
+  height: 100%;
+  width: fit-content;
+  display: flex;
+  flex-grow: 1;
+  align-items: center;
+  transition: opacity 0.5s ease-out;
+  background-position: 0 -200px;
+  animation: Slider 100s linear infinite;
+`;
+
+const Story = styled.div`
+  height: 82px;
+  width: 88px;
+  background-color: #1f8f64;
+  margin: 0 10px;
 `;
 
 type StoriesProps = {};
 
 const Stories = ({}: StoriesProps) => {
-  return <Container>This is Stories Component/Page</Container>;
+  return (
+    <Container>
+      <Main>
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+      </Main>
+    </Container>
+  );
 };
 
 export default Stories;
