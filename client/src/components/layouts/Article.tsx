c3be767f84b1affc11733589f99a9f2d691f9f85
MedElBoudali <med.sneaky@gmail.com>
2021-01-13 20:27:21 +0000

    ✔ likes and description

--- client/src/components/layouts/Article.tsx
+++ client/src/components/layouts/Article.tsx
@@ -1,5 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
+import { useMeQuery } from '../../generated/graphql';
 
 const Container = styled.article`
   background-color: #fff;
@@ -117,6 +118,42 @@ const IconsContainer = styled.section`
   }
 `;
 
+const LikesCss = css`
+  color: #262626;
+  margin: 0 3px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+const LikesContainer = styled.section`
+  margin-bottom: 8px;
+`;
+
+const LikesLink = styled(Link)`
+  text-decoration: none;
+  ${LikesCss}
+`;
+
+const OtherButton = styled.button`
+  border: 0;
+  padding: 0;
+  background: none;
+  ${LikesCss}
+`;
+
+const DescriptionAndCommentsContainer = styled.div``;
+
+const Description = styled.div`
+  display: flex;
+  margin-bottom: 4px;
+  color: #262626;
+  a {
+    color: #262626;
+    text-decoration: none;
+    font-weight: 600;
+    margin-right: 4px;
+  }
+`;
+
 type PostProps = {
   name: string;
   logo: string;
@@ -138,9 +175,10 @@ const Post = ({
   commentsLength,
   createdTime
 }: PostProps) => {
+  const { data } = useMeQuery();
   const onClick = (buttonName: string) => {
     // TODO: Edit this later
-    console.log(`${buttonName} Button Click.`);
+    console.log(`${buttonName} Button Clicked.`);
   };
 
   return (
@@ -168,14 +206,14 @@ const Post = ({
       <Details>
         <IconsContainer>
           <span>
-            <button onClick={() => onClick('Like')}>
+            <button type='button' onClick={() => onClick('Like')}>
               <svg fill='#262626' height='24' viewBox='0 0 48 48' width='24'>
                 <path d='M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z'></path>
               </svg>
             </button>
           </span>
           <span>
-            <button onClick={() => onClick('Comment')}>
+            <button type='button' onClick={() => onClick('Comment')}>
               <svg fill='#262626' height='24' viewBox='0 0 48 48' width='24'>
                 <path
                   clipRule='evenodd'
@@ -185,20 +223,42 @@ const Post = ({
             </button>
           </span>
           <span>
-            <button onClick={() => onClick('Share')}>
+            <button type='button' onClick={() => onClick('Share')}>
               <svg fill='#262626' height='24' viewBox='0 0 48 48' width='24'>
                 <path d='M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z'></path>
               </svg>
             </button>
           </span>
           <span>
-            <button onClick={() => onClick('Save')}>
+            <button type='button' onClick={() => onClick('Save')}>
               <svg fill='#262626' height='24' viewBox='0 0 48 48' width='24'>
                 <path d='M43.5 48c-.4 0-.8-.2-1.1-.4L24 29 5.6 47.6c-.4.4-1.1.6-1.6.3-.6-.2-1-.8-1-1.4v-45C3 .7 3.7 0 4.5 0h39c.8 0 1.5.7 1.5 1.5v45c0 .6-.4 1.2-.9 1.4-.2.1-.4.1-.6.1zM24 26c.8 0 1.6.3 2.2.9l15.8 16V3H6v39.9l15.8-16c.6-.6 1.4-.9 2.2-.9z'></path>
               </svg>
             </button>
           </span>
         </IconsContainer>
+        <LikesContainer>
+          <div>
+            Liked by
+            <span>
+              <LikesLink to={`/${data?.me?.userName === liked ? '/profile' : liked}`}>
+                {liked}
+              </LikesLink>
+            </span>
+            and
+            <OtherButton type='button' onClick={() => onClick('Others')}>
+              others.
+            </OtherButton>
+          </div>
+        </LikesContainer>
+        <DescriptionAndCommentsContainer>
+          <Description>
+            <span>
+              <Link to={`/${name}`}>{name}</Link>
+              {description}
+            </span>
+          </Description>
+        </DescriptionAndCommentsContainer>
       </Details>
     </Container>
   );
