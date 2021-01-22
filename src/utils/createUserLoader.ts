1b07f5c5bad31da52f0241b29d4516eacac0982e
MedElBoudali <med.sneaky@gmail.com>
2021-01-22 17:00:07 +0000

    ✔ trying to deploy to heroku

--- src/utils/createUserLoader.ts
+++ src/utils/createUserLoader.ts
@@ -1,5 +1,5 @@
 import DataLoader from 'dataloader';
-import { User } from '../entities/User';
+import { User } from '../entities/user';
 
 export const createUserLoader = () =>
   new DataLoader<number, User>(async userIds => {
