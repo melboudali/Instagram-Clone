b39cea380c30c29bde85e1c816429a87c32b0c2d
MedElBoudali <med.sneaky@gmail.com>
2021-02-11 21:31:03 +0000

    🚀 heroku deploy

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ const main = async () => {
 	const redis = new Redis(process.env.REDIS_URL);
 
 	app.set("trust proxy", 1);
-	app.use(cors({ origin: process.env.CORS_DOMAIN, credentials: true }));
+	app.use(cors({ origin: process.env.CORS_DOMAIN!, credentials: true }));
 	app.use(
 		session({
 			name: cookieName,
@@ -78,7 +78,7 @@ const main = async () => {
 
 	if (isProd) {
 		app.use(express.static(path.join(__dirname, "../client/build")));
-		app.get("*", (_, res) => {
+		app.get("/", (_, res) => {
 			res.sendFile(path.join(__dirname, "../client/build/index.html"));
 		});
 	}
