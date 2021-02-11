75275d9a8bfaf2bf260afb9f1becdd2ab7f5d033
MedElBoudali <med.sneaky@gmail.com>
2021-02-11 00:53:14 +0000

    🚀 trying to deploy to heroku

--- src/index.ts
+++ src/index.ts
@@ -75,9 +75,9 @@ const main = async () => {
 	});
 
 	if (isProd) {
-		app.use(express.static(path.join(__dirname, "..client/build")));
+		app.use(express.static(path.join(__dirname, "client/build")));
 		app.get("*", (_, res) => {
-			res.sendFile(path.join(__dirname, "../client/build/index.html"));
+			res.sendFile(path.join(__dirname, "client/build/index.html"));
 		});
 	}
 
