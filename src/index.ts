2705c613c517f81ea9217bbe8ce0c94bfdb33c71
MedElBoudali <med.sneaky@gmail.com>
2021-02-14 00:22:28 +0000

    🚀 trying to run migrations on heroku

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import "dotenv/config";
 import express, { Request, Response, NextFunction } from "express";
-import { createConnection } from "typeorm";
+import { Connection, createConnection } from "typeorm";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { cookieName, isProd } from "./config/constants";
@@ -25,15 +25,17 @@ const PORT = process.env.PORT || 5000;
 const main = async () => {
 	const app = express();
 
-	await createConnection({
+	const connection: Connection = await createConnection({
 		type: "postgres",
 		url: process.env.DATABASE_URL,
 		synchronize: false,
 		logging: !isProd,
-		ssl: isProd && { rejectUnauthorized: false },
+		ssl: isProd ? { rejectUnauthorized: false } : false,
 		entities: [User, Image, Like, Comment, Follower],
 		migrations: [path.join(__dirname, "migrations/*.js")]
 	});
+
+	await connection.runMigrations();
 
 	const RedisStore = connectRedis(session);
 	const redis = new Redis(process.env.REDIS_URL);
