74571cf564d2e598ce4a07ebd18b9286df15c89e
MedElBoudali <med.sneaky@gmail.com>
2021-04-05 13:01:12 +0000

    🎨 added seo and helmet

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,9 +2,13 @@ import Container from "../containers/Container";
 import FileUploadInputProps from "../components/home/FileUploadInput";
 import Stories from "../components/home/stories/Stories";
 import ArticlesAndSuggestionsContainer from "../components/home/ArticlesAndSuggestionsContainer";
+import { Helmet } from "react-helmet";
 
 const Home = () => (
 	<Container>
+		<Helmet>
+			<title>Instagram Clone</title>
+		</Helmet>
 		<FileUploadInputProps />
 		<Stories />
 		<ArticlesAndSuggestionsContainer />
